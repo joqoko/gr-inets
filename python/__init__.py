8c80bef1220d209ba0a948fd2e7675591f6a1457
pwa <joqoko@hotmail.com>
2016-11-17 16:10:56 +0100

    FirstCommitAfterRedo-framing_py-Done

--- python/__init__.py
+++ python/__init__.py
@@ -36,6 +36,8 @@ from unmake_packet_python import unmake_packet_python
 from stop_and_wait_arq import stop_and_wait_arq
 from rrrm import rrrm
 from per_logger import per_logger
+from framing_py_v1 import framing_py_v1
+
 
 
 #
