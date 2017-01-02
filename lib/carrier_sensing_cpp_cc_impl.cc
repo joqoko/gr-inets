30dde564f3066ae59e0a21ea9c6a203e7726fc8b
inets <joqoko@hotmail.com>
2017-01-02 17:23:27 +0100

    HalfwayCS

--- lib/carrier_sensing_cpp_cc_impl.cc
+++ lib/carrier_sensing_cpp_cc_impl.cc
@@ -71,46 +71,134 @@ namespace gr {
 
     void carrier_sensing_cpp_cc_impl::start_sensing(pmt::pmt_t info_in)
     {
-      // std::cout << "received a message" << std::endl;
-      if(pmt::is_dict(info_in))
+      if(_cs_mode == 1)
       {
-        if(_develop_mode == 1 || _develop_mode == 2)
-          std::cout << "+++++++++ carrier sensing ID: " << _block_id << "  +++++++++++++++" << std::endl;    
-        // this function is fired
-        pmt::pmt_t not_found;
-        int frame_type = pmt::to_long(pmt::dict_ref(info_in, pmt::string_to_symbol("frame_type"), not_found)); 
-        if(frame_type == 1)
+        if(pmt::is_dict(info_in))
         {
-          _frame_info = info_in;
-          _in_cca = true;
-          if(_develop_mode == 1)
-            std::cout << "before sending a data frame, start sensing" << std::endl;
-          boost::thread thrd(&carrier_sensing_cpp_cc_impl::countdown_sensing, this);
+          if(_develop_mode == 1 || _develop_mode == 2)
+            std::cout << "+++++++++ cs ID: " << _block_id << " in mode Oneshot  +++++++++" << std::endl;    
+          // this function is fired
+          pmt::pmt_t not_found;
+          int frame_type = pmt::to_long(pmt::dict_ref(info_in, pmt::string_to_symbol("frame_type"), not_found)); 
+          if(frame_type == 1)
+          {
+            _frame_info = info_in;
+            _in_cca = true;
+            if(_develop_mode == 1)
+              std::cout << "before sending a data frame, start sensing" << std::endl;
+            boost::thread thrd(&carrier_sensing_cpp_cc_impl::oneshot_sensing, this);
+          }
+          else
+          {
+            if(_develop_mode == 1)
+              std::cout << "before sending a ack frame, no sensing" << std::endl;
+            message_port_pub(pmt::mp("frame_info_pass_out"), info_in);
+          }
+        }
+        else if(pmt::is_real(info_in))
+        {
+          double power = pmt::to_double(info_in);
+          _in_cca = (_cs_threshold > power);
+          if(_develop_mode == 1 && !_in_cca)
+          {
+            struct timeval t;
+            gettimeofday(&t, NULL);
+            double current_time = t.tv_sec - double(int(t.tv_sec/100)*100) + t.tv_usec / 1000000.0;
+            std::cout << "in oneshot carrier sensing, average rx power is: " << power << ", received at " << current_time << " s" << std::endl;
+          }
         }
         else
         {
-          if(_develop_mode == 1)
-            std::cout << "before sending a ack frame, no sensing" << std::endl;
-          message_port_pub(pmt::mp("frame_info_pass_out"), info_in);
+          // not a dict pmt, most likely an import error
+          std::cout << "Warning: is not a valid input pmt. Please check your connections." << std::endl;
         }
       }
-      else if(pmt::is_real(info_in))
+      else if(_cs_mode == 2)
       {
-        double power = pmt::to_double(info_in);
-        _in_cca = (_cs_threshold > power);
-        if(_develop_mode == 1 && !_in_cca)
+        if(pmt::is_dict(info_in))
         {
-          struct timeval t;
-          gettimeofday(&t, NULL);
-          double current_time = t.tv_sec - double(int(t.tv_sec/100)*100) + t.tv_usec / 1000000.0;
-          std::cout << "in carrier sensing, average rx power is: " << power << ", received at " << current_time << " s" << std::endl;
+          if(_develop_mode == 1 || _develop_mode == 2)
+            std::cout << "+++++++++ cs ID: " << _block_id << " in mode Fix duration +++++++++" << std::endl;    
+          // this function is fired
+          pmt::pmt_t not_found;
+          int frame_type = pmt::to_long(pmt::dict_ref(info_in, pmt::string_to_symbol("frame_type"), not_found)); 
+          if(frame_type == 1)
+          {
+            _frame_info = info_in;
+            _in_cca = true;
+            if(_develop_mode == 1)
+              std::cout << "before sending a data frame, start sensing" << std::endl;
+            boost::thread thrd(&carrier_sensing_cpp_cc_impl::countdown_sensing, this);
+          }
+          else
+          {
+            if(_develop_mode == 1)
+              std::cout << "before sending a ack frame, no sensing" << std::endl;
+            message_port_pub(pmt::mp("frame_info_pass_out"), info_in);
+          }
         }
+        else if(pmt::is_real(info_in))
+        {
+          double power = pmt::to_double(info_in);
+          _in_cca = (_cs_threshold > power);
+          if(_develop_mode == 1 && !_in_cca)
+          {
+            struct timeval t;
+            gettimeofday(&t, NULL);
+            double current_time = t.tv_sec - double(int(t.tv_sec/100)*100) + t.tv_usec / 1000000.0;
+            std::cout << "in carrier sensing, average rx power is: " << power << ", received at " << current_time << " s" << std::endl;
+          }
+        }
+        else
+        {
+          // not a dict pmt, most likely an import error
+          std::cout << "Warning: is not a valid input pmt. Please check your connections." << std::endl;
+        }
+      }
+      else if(_cs_mode == 2)
+      {
+        
+      }
+    }
+
+    void carrier_sensing_cpp_cc_impl::oneshot_sensing()
+    {
+      _in_cca = true;
+      struct timeval t;
+      gettimeofday(&t, NULL);
+      double current_time = t.tv_sec + t.tv_usec / 1000000.0;
+      double start_time = t.tv_sec + t.tv_usec / 1000000.0;
+      // std::cout << "start_countdown" << std::endl;
+      // std::cout << "cs duration is: " << _cs_duration << " in second: " << _cs_duration /1000 << std::endl;
+      // std::cout << "current time is: " << current_time << std::endl;
+      // std::cout << "_in_cca is: " << _in_cca << std::endl;
+      // std::cout << "time condition is: " << (start_time + (_cs_duration / 1000) - current_time) << std::endl;
+      // std::cout << ((current_time < start_time + _cs_duration / 1000) && _in_cca) << std::endl;
+      while((current_time < start_time + _cs_duration / 1000) && _in_cca)
+      {
+        boost::this_thread::sleep(boost::posix_time::microseconds(_system_time_granularity_us)); 
+        gettimeofday(&t, NULL);
+        current_time = t.tv_sec + t.tv_usec / 1000000.0;
+//        std::cout << "current sensing time is: " << current_time - start_time - _cs_duration / 1000 << std::endl;
+        // std::cout << "sensing status is: " << _in_cca << std::endl;
+//        std::cout << "in sensing " << start_time + _cs_duration / 1000 - current_time << std::endl;
+      }
+      if(_in_cca)
+      {
+        if(_develop_mode == 1)
+          std::cout << "Carrier sensing passed. " << std::endl;
+        message_port_pub(pmt::mp("frame_info_pass_out"), _frame_info);
       }
       else
       {
-        // not a dict pmt, most likely an import error
-        std::cout << "Warning: is not a valid input pmt. Please check your connections." << std::endl;
+        if(_develop_mode == 1)
+          std::cout << "Carrier sensing failed. " << std::endl;
+        message_port_pub(pmt::mp("frame_info_fail_out"), _frame_info);
       }
+      _in_cca = false;
+      _cs_time = current_time - start_time;
+      if(_develop_mode == 1)
+        std::cout << "Carrier sensing time is: " << _cs_time << " s" << std::endl;
     }
 
     void carrier_sensing_cpp_cc_impl::countdown_sensing()
